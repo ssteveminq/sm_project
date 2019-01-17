02b36409a4bfa5d71f54b813f4492100aa397209
ssteveminq <steveminq@gmail.com>
2019-01-17 17:12:24 -0600

    Initial Implementation of SM: S0 to S1 caused to break computer

--- scripts/hsr_state_machine.py
+++ scripts/hsr_state_machine.py
@@ -10,18 +10,49 @@ from smach import State
 import tf.transformations
 
 
-def generate_commanded_state(userdata):
+def get_action(cmd_idx):
+
+    if cmd_idx==len(desired_staes):
+        print "index exceeds the lenght of array"
+        output_state = 'end_demo'
+        return output_state
+
+    desired_state=desired_states[cmd_idx]
+    
+    if desired_state == '0':
+        output_state = 'Go_S0'
+    elif desired_state == '1':
+        output_state = 'Go_S1'
+    elif desired_state == '2':
+        output_state = 'Go_S2'
+    elif desired_state == '3':
+        output_state = 'Go_S3'
+    elif desired_state == '4':
+        output_state = 'Go_S4'
+    else:  
+        print "desired state out of bounds"
+        output_state = 'end_demo'
+
+    return output_state
+
+#from cmd_idx with desired_states_array, set the goal position 
+def generate_send_goal(cmd_idx):
+
+       if cmd_idx ==-1:
+           return GoalStatus.SUCCEEDED
 
 	goal_y = 0.4
 	goal_yaw = 0.0	
 
-	if userdata == '0':
+        cmd_state = desired_staes[cmd_idx]
+
+	if cmd_state == '0':
 		goal_x = 4.0
-	elif userdata == '1':
+	elif cmd_state == '1':
 		goal_x = 3.5
-	elif userdata == '2':
+	elif cmd_state == '2':
 		goal_x = 3.0
-	elif userdata == '3':
+	elif cmd_state == '3':
 		goal_x = 2.5
 	else:  
 		goal_x = 2.0
@@ -51,46 +82,65 @@ def generate_commanded_state(userdata):
 
 
 
-class POS_0(smach.State):
+class S_0(smach.State):
 	def __init__(self):
-		smach.State.__init__(self, outcomes = ['left', 'right', 'stay', 'end_demo'])
+		smach.State.__init__(self, outcomes = ['Go_S0', 'GO_S1' , 'end_demo'])
+                                    input_keys=['S0_counter_in'],
+                                    output_keys=['S0_counter_out'])
 
 
 	def execute(self, userdata):
 		rospy.loginfo('Moving to state 0')
+                
+                cmd_idx=userdata.S0_counter_in
+
+                start_time = rospy.Time()
+                duration=0
+
+                while duration<10.0:
+
+                    action_state = generate_send_goal(cmd_idx)
+
+                    curr_time =rospy.Time()
+                    duration = curr_time - start_time
+                    print "duration time: ", duration + "action_state", action_state
+
+                    if action_state == GoalStatus.SUCCEEDED:
+                        cmd_idx= -1
 
 
-		action_state = generate_commanded_state(goal_x, goal_y, goal_yaw)
+                if action_state == GoalStatus.SUCCEEDED:
+                    userdata.S0_counter_out=userdata.S0_counter_in+1
+                    return get_action(userdata.S0_counter_out)
+                else
+                    print action_state
 
-		if (less than 10 s have passed):
-			if action_state == GoalStatus.SUCCEEDED:
-				return 'stay'
-			else:
-				return 'left'
-		# 10 seconds have passed
-		else: 
-			state_index+=1
-			if state_index == length_state_array
-				return 'end_demo'
-			else:
-				goal_state = state_array[state_index]
-				if goal_state == '0'
-					return 'stay'
-				else:
-					return 'right'
+                    return 'end_demo'
+                    
+                # if state_index == length_state_array
+                                # return 'end_demo'
 
 
-class POS_1(smach.State):
+
+
+class S_1(smach.State):
 	def __init__(self):
-		smach.State.__init__(self, outcomes = ['left', 'right', 'stay', 'end_demo'])
+		smach.State.__init__(self, outcomes = ['Go_S0','Go_S1', 'GO_S2', 'end_demo'],
+                                           input_keys=['S1_counter_in'],
+                                           output_keys=['S1_counter_out'])
 
+        def execute(self, userdata):
+            rospy.loginfo('Executing state S_1')
+            rospy.loginfo('Counter = %f'%userdata.S1_counter_in)
+            return 'end_demo'
+     
 
-class POS_2(smach.State):
+class S_2(smach.State):
 	def __init__(self):
-		smach.State.__init__(self, outcomes = ['left', 'right', 'stay', 'end_demo'])
+		smach.State.__init__(self, outcomes = ['Go_S0', 'GO_S2', 'end_demo'])
 
 
-class POS_3(smach.State):
+class S_3(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes = ['left', 'right', 'stay', 'end_demo'])
 
@@ -109,11 +159,35 @@ rospy.init_node('test_move')
 
 #===========Read csv==============
 
-state_array = []
-state_index = 0
+f = open('../csv_parser/states_and_vals.csv', 'rt')
+reader = csv.reader(f)
+row_count = sum(1 for row in reader)
+f.seek(0)
+
+col_count = len(next(reader))
+# print "columns"
+# print col_count
+
+f.seek(0)
+data = [row for row in reader] # list comprehension 
+f.close()
+string_col = data[0]
+desired_states = []
+
+for col in range(0, col_count):
+	if (string_col[col] == 'robot_state'):
+		robot_state_col = col 
+
+for row1 in range(1,row_count):
+	this_row = data[row1]
+	desired_states.extend([this_row[robot_state_col]])
+
+desired_states = np.array(desired_states)
+desired_states.resize(desired_states.shape[0])
+
+print desired_states
 
 #==================================
-
 
 # initialize action client
 cli = actionlib.SimpleActionClient('/move_base/move', MoveBaseAction)
@@ -123,10 +197,20 @@ cli.wait_for_server()
 
 # create SMACH state machine
 sm = smach.StateMachine(outcomes=['stop'])
+# sm.userdata.desired_states =desired_states
+sm.userdata.state_index=0
 
 with sm:
-	smach.StateMachine.add('POS_0', POS_0(),
-		transitions = {'left': 'POS_0', 'right' : 'POS_1', 'stay': 'POS_0', 'end_demo' : 'stop'})
+	smach.StateMachine.add('S_0', S_0(),
+		transitions = {'Go_S0': 'S_0', 'Go_S1' : 'S_1', 'end_demo' : 'stop'},
+                remapping = {'S0_counter_in':'state_index',
+                             'S0_counter_out':'state_index'})
+	smach.StateMachine.add('S_1', S_1(),
+		transitions = {'Go_S0': 'S_0', 'Go_S1':'S_1', 'Go_S2':'S_2', 'end_demo':'stop'},
+                remapping = {'S1_counter_in':'state_index',
+                             'S1_counter_out':'state_index'})
+
+
 
 outcome = sm.execute()
 
