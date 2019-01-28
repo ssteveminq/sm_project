a0278d8d3379e36a9c5049a9c1abfa8b8dcfb720
ssteveminq <steveminq@gmail.com>
2019-01-28 17:06:43 -0600

    modified client

--- socket/client_tcp.py
+++ socket/client_tcp.py
@@ -1,17 +1,24 @@
 import socket
+from platform import python_version
 
 
 #input(): for above python 3.x
 #raw_input(): for above python 2.x
 
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 4999  # socket server port number
+    # host = socket.gethostname()  # as both code is running on same pc
+    host = '10.157.137.49'# as both code is running on same pc
+    port = 4998  # socket server port number
 
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
 
-    message = raw_input(' -> ')  # take input
+    version_int= int(python_version()[0])
+    if(version_int>0):
+        message = input(' -> ')  # take input
+    else:
+        message = raw_input(' -> ')  # take input
+
 
     # while message.lower().strip() != 'bye':
     while message.lower() != 'bye':
@@ -20,7 +27,11 @@ def client_program():
         # data = client_socket.recv(1024).decode()  # receive response
 
         # print('Received from server: ' + data)  # show in terminal
-        message = raw_input(" -> ")  # again take input
+        message = input(" -> ")  # again take input
+        if(version_int>0):
+            message = input(' -> ')  # take input
+        else:
+            message = raw_input(' -> ')  # take input
 
     client_socket.close()  # close the connection
 
