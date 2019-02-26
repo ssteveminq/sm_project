1c0fe4f85d8978322d16fb27bf63c2231a144f73
Rachel <rachel.schlossman@utexas.edu>
2019-02-25 21:21:59 -0600

    plotting for 30 min data

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -13,9 +13,9 @@ def map_states(row):
 
 def main():
 
-	data = np.genfromtxt('results.csv',delimiter=',', dtype = float)
+	data = np.genfromtxt('02_25_30_min.csv',delimiter=',', dtype = float)
 
-	time = [row[0] for row in data]
+	time = [row[0]/60 for row in data]
 	state = [row[1] for row in data]
 	workload = [row[2]/30 for row in data]
 	complete_work_with_robot = [row[3] for row in data]
@@ -25,23 +25,28 @@ def main():
 	obstacle2 = [row[7]*2 for row in data]
 	human_at_start_of_check = [row[8] for row in data]
 
-	print workload
+	# print workload
 
-	print time 
+	# print time 
 	map_states(state)
 
-	plt.figure(1)
-	plt.plot(time, state, 'k--', label='state')
-	plt.scatter(time,state,c=cm.magma(complete_work_with_robot))
-	plt.plot(time, workload, '--', label='workload')
-	#plt.scatter(time,workload)
-	plt.plot(time, obstacle1, 'o', label='obstacle1')
-	plt.plot(time, obstacle2, 'o', label='obstacle2')
+	# plt.figure(1)
+	# plt.plot(time, state, 'k-', label='state')
+	# plt.plot(time, state, 'k--', label='state')
+	# plt.scatter(time,state,c='k')
+
+	# plt.scatter(time,state,c=cm.magma(complete_work_with_robot))
+	plt.plot(time, workload, label='workload')
+	plt.scatter(time,workload)
+	# plt.plot(time, obstacle1, 'o', label='obstacle1')
+	# plt.plot(time, obstacle2, 'o', label='obstacle2')
 	plt.xlabel('time (s)')
-	plt.legend()
+	# plt.legend()
 	plt.grid()
 	axes = plt.gca()
-	axes.set_ylim([-.1,3.1])
+	# axes.set_ylim([-.1,3.1])
+	axes.set_ylim([.2,.9])
+	axes.set_xlim([0,34])
 
 	plt.show()
 
